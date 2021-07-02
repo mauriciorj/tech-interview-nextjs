f01286d421d5b447cc92354410484c4a4c00a0d8
mauricio.o.mesquita@gmail.com <mauricio.o.mesquita@gmail.com>
2021-07-01 22:38:43 -0400

    Hotfix: breadcrum - item, typescript

--- src/components/breadCrumbs/index.tsx
+++ src/components/breadCrumbs/index.tsx
@@ -4,7 +4,7 @@ import { Breadcrumbs, Link, Typography } from '@material-ui/core';
 const BreadCrumbs = ({ breadCrumbInfo }: any) => {
     return (
         <Breadcrumbs aria-label="breadcrumb">
-            {breadCrumbInfo.map((item) => (
+            {breadCrumbInfo.map((item: { link: any; title: string | null; }) => (
                 item.link ? (
                     <Link color="inherit" href="/" key={item.title}>
                         {item.title}
